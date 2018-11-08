3cb2027e65072432d9c7fcf7a5acd2f9537c8cb2
sepehr <sepehrhashtroudi@gmail.com>
2018-11-09 00:46:35 +0330

    working

--- mnist.py
+++ mnist.py
@@ -3,10 +3,10 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 
 
-n_epoch = 5000
+n_epoch = 500
 batch_size = 500
-Data_len = 3500
 loss_list =[]
+test_accuracy_list =[]
 
 
 def load_Data():
@@ -35,39 +35,53 @@ def plot_digit(some_digit):
     plt.axis("off")
     plt.show()
 
-def plot_loss(loss_list):
-    plt.plot(loss_list)
+def plot_loss_acc(loss_acc_list):
+    plt.plot(loss_acc_list)
     plt.show()
+
+def accuracy(test_data, test_target, w , b):
+    error = 0
+    for i in range(len(test_target)):
+        predicted_target = np.round(1.0 / (1 + np.exp(np.matmul(test_data[i], w) + b)))
+        if(predicted_target != test_target[i] ):
+            error += 1
+    return 100*error/len(test_target)
 
 def log_reg():
     X = tf.placeholder(dtype=tf.float64, shape=[None, 784], name="x")
     Y = tf.placeholder(dtype=tf.float64, shape=[None,1], name="y")
     w = tf.Variable(tf.random_normal(shape=[784, 1], stddev=0.01, dtype=tf.float64), name="weights", dtype=tf.float64)
     b = tf.Variable(0.0, dtype=tf.float64)
-    Y_predicted = 1.0 / ( tf.exp( tf.matmul(X , w) + b))
-    print("a", x_train[1])
-    loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=Y, logits=Y_predicted))
-    optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001).minimize(loss)
+    Y_predicted = 1.0 / (1 + tf.exp( tf.matmul(X , w) + b))
+    # print("a", x_train[1])
+    loss = tf.reduce_sum(tf.nn.sigmoid_cross_entropy_with_logits(labels=Y, logits=Y_predicted))
+
+    # eps = 1e-10
+    # maximum_likelihood = Y*tf.log(Y_predicted+eps) + (1-Y)*tf.log(1-Y_predicted+eps)
+    # loss = -1 * tf.reduce_sum(maximum_likelihood)
+
+
+    # loss = tf.reduce_sum(tf.nn.sigmoid_cross_entropy_with_logits(labels=Y, logits=Y_predicted))
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.00001).minimize(loss)
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
         for i in range(n_epoch):  # run 100 epochs
-            for idx in range(int(Data_len/batch_size)):
+            for idx in range(int(len(x_train)/batch_size)):
                 Input_list = {X: x_train[idx*batch_size:(idx+1)*batch_size], Y: y_train[idx*batch_size:(idx+1)*batch_size]}
-                _,Loss = sess.run([optimizer,loss], feed_dict=Input_list)
+                _,Loss,w_value,b_value = sess.run([optimizer,loss,w,b], feed_dict=Input_list)
                 loss_list. append(Loss)
+            test_accuracy_list.append(accuracy(x_test, y_test, w_value, b_value))
 
-        w_value, b_value = sess.run([w, b])
-        print(w_value,b_value)
+
+        # print(w_value,b_value)
+        return w_value , b_value
 
 
 
 
 
 x_train, y_train, x_valid, y_valid, x_test, y_test = load_Data()
-
-plot_digit(x_test[1])
-print(x_train.shape)
-
-log_reg()
-plot_loss(loss_list)
-
+# plot_digit(x_test[1])
+w,b =log_reg()
+plot_loss_acc(loss_list)
+plot_loss_acc(test_accuracy_list)
